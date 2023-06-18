b1fae1e024c1c4e140a3b6c0b3e3009dd593db66
shashank <shashankmbhat@gmail.com>
2023-06-18 20:00:49 +0530

    Data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,9 +12,9 @@ from src.components.data_transformation import DataTransformation,DataTransforma
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifact',"train.csv")
-    test_data_path: str = os.path.join('artifact',"test.csv")
-    raw_data_path: str = os.path.join('artifact',"data.csv")
+    train_data_path: str = os.path.join('artifacts',"train.csv")
+    test_data_path: str = os.path.join('artifacts',"test.csv")
+    raw_data_path: str = os.path.join('artifacts',"data.csv")
     
 class DataIngestion:
     def __init__(self):
