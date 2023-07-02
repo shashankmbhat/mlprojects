de30f27d1a7a0544c63ab9424e9aac98653cac98
shashank <shashankmbhat@gmail.com>
2023-07-02 19:19:26 +0530

    prediction pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,7 +8,11 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig,ModelTrianer
 
+import warnings
+warnings.filterwarnings("ignore")
+from sklearn.exceptions import FitFailedWarning 
 
 @dataclass
 class DataIngestionConfig:
@@ -49,4 +53,7 @@ if __name__ == "__main__":
     
     data_transformation = DataTransformation()
     train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    modeltrainer = ModelTrianer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
             
